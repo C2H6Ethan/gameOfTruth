c8aec0953c60c1698f381312082419bed5a42ce9
C2H6Ethan <ejbaum@live.com>
2023-03-28 22:52:26 +0200

    changed privacy policy links

--- src/screens/PaywallScreen.js
+++ src/screens/PaywallScreen.js
@@ -1,48 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Modal, Text, View, Image, SafeAreaView, TouchableOpacity, Linking, Platform } from 'react-native';
-import CustomButton from '../components/CustomButton';
-import Constants from 'expo-constants';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ThemeProvider } from '@react-navigation/native';
-const translations = require('../translations.json');
+import {
+  StyleSheet,
+  Modal,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  Linking,
+  Platform,
+} from "react-native";
+import CustomButton from "../components/CustomButton";
+import Constants from "expo-constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ThemeProvider } from "@react-navigation/native";
+const translations = require("../translations.json");
 
 export default function PaywallScreen({ navigation }) {
-  const [price, setPrice] = useState('');
+  const [price, setPrice] = useState("");
   const [language, setLanguage] = useState("en");
 
-  var iapID = Platform.OS == 'ios'? 'com.gameOfTruth.premium': 'com.gameoftruth.premium'
-  
-  useEffect(async() => {
-    getIAPDetails()
-    var language = await AsyncStorage.getItem('language')
-    if(language){setLanguage(language)}
+  var iapID =
+    Platform.OS == "ios"
+      ? "com.gameOfTruth.premium"
+      : "com.gameoftruth.premium";
+
+  useEffect(async () => {
+    getIAPDetails();
+    var language = await AsyncStorage.getItem("language");
+    if (language) {
+      setLanguage(language);
+    }
   }, []);
 
-  const getIAPDetails = async() => {
-    if (Constants.appOwnership != "expo"){
-      const InAppPurchases = await import("expo-in-app-purchases")
-  
+  const getIAPDetails = async () => {
+    if (Constants.appOwnership != "expo") {
+      const InAppPurchases = await import("expo-in-app-purchases");
+
       await InAppPurchases.connectAsync();
-  
-      var premium = await InAppPurchases.getProductsAsync([iapID])
-      setPrice(premium.results[0]['price'])
+
+      var premium = await InAppPurchases.getProductsAsync([iapID]);
+      setPrice(premium.results[0]["price"]);
 
       await InAppPurchases.disconnectAsync();
     }
-  }
+  };
 
-  const purchasePremium = async() => {
-    if (Constants.appOwnership == "expo") return false
+  const purchasePremium = async () => {
+    if (Constants.appOwnership == "expo") return false;
     const InAppPurchases = await import("expo-in-app-purchases"),
-      { IAPResponseCode } = await import('expo-in-app-purchases');
+      { IAPResponseCode } = await import("expo-in-app-purchases");
 
     try {
       await InAppPurchases.connectAsync();
 
-      await InAppPurchases.getProductsAsync([iapID])
+      await InAppPurchases.getProductsAsync([iapID]);
 
-      InAppPurchases.purchaseItemAsync(iapID)
+      InAppPurchases.purchaseItemAsync(iapID);
 
       return await new Promise((resolve, reject) => {
         InAppPurchases.setPurchaseListener(async (result) => {
@@ -63,40 +78,38 @@ export default function PaywallScreen({ navigation }) {
               await InAppPurchases.disconnectAsync();
               return reject(new Error("IAP Error: " + result.errorCode));
           }
-        })
-      })
-      
+        });
+      });
     } catch (e) {
       await InAppPurchases.disconnectAsync();
       throw e;
     }
+  };
 
-  }
-
-  const onSuccess = async() => {
-    await AsyncStorage.setItem('hasUpgraded', 'true');
+  const onSuccess = async () => {
+    await AsyncStorage.setItem("hasUpgraded", "true");
     navigation.reset({
       index: 0,
-      routes: [{ name: 'PremiumThankYou' }],
+      routes: [{ name: "PremiumThankYou" }],
     });
-  }
+  };
 
-  const restorePurchase = async() => {
-    if (Constants.appOwnership == "expo") return false
-    const InAppPurchases = await import("expo-in-app-purchases")
+  const restorePurchase = async () => {
+    if (Constants.appOwnership == "expo") return false;
+    const InAppPurchases = await import("expo-in-app-purchases");
 
     try {
       await InAppPurchases.connectAsync();
 
       var results = await InAppPurchases.getPurchaseHistoryAsync();
-      results = results['results']
+      results = results["results"];
 
       for (var i = 0; i < results.length; i++) {
         var result = results[i];
-        if(result.productId == iapID && result.acknowledged) {
-          onSuccess()
+        if (result.productId == iapID && result.acknowledged) {
+          onSuccess();
           await InAppPurchases.disconnectAsync();
-          return true
+          return true;
         }
       }
       await InAppPurchases.disconnectAsync();
@@ -104,48 +117,184 @@ export default function PaywallScreen({ navigation }) {
       await InAppPurchases.disconnectAsync();
       throw e;
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Image style={{position: 'absolute', width: '100%', height: '100%'}} source={require('../assets/paywallBackground.png')}/>
+      <Image
+        style={{ position: "absolute", width: "100%", height: "100%" }}
+        source={require("../assets/paywallBackground.png")}
+      />
       <View style={styles.content}>
-        <TouchableOpacity onPress={() => navigation.goBack()} activeOpacity={.7}>
-          <Image style={styles.closeButton} source={require('../assets/closeButton.png')} /> 
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          activeOpacity={0.7}
+        >
+          <Image
+            style={styles.closeButton}
+            source={require("../assets/closeButton.png")}
+          />
         </TouchableOpacity>
-        <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 30, lineHeight: 36, color: 'white'}}>{translations[language]["Support our work & get a better party experience."]}</Text>
+        <Text
+          style={{
+            fontFamily: "Gilroy-ExtraBold",
+            fontSize: 30,
+            lineHeight: 36,
+            color: "white",
+          }}
+        >
+          {
+            translations[language][
+              "Support our work & get a better party experience."
+            ]
+          }
+        </Text>
 
         <View style={styles.infoContainer}>
           <View style={styles.info}>
             <View style={styles.infoTitle}>
-              <Image style={{width: 36, height: 36, marginRight: 18}} source={require('../assets/video-game-key.png')}/>
-              <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 24, lineHeight: 29, color: 'white'}}>{translations[language]["Access all packages!"]}</Text>
+              <Image
+                style={{ width: 36, height: 36, marginRight: 18 }}
+                source={require("../assets/video-game-key.png")}
+              />
+              <Text
+                style={{
+                  fontFamily: "Gilroy-ExtraBold",
+                  fontSize: 24,
+                  lineHeight: 29,
+                  color: "white",
+                }}
+              >
+                {translations[language]["Access all packages!"]}
+              </Text>
             </View>
-            <Text style={{fontFamily: 'Poppins', fontSize: 16, lineHeight: 21, color: 'white', flexWrap: 'wrap', marginLeft: 54}}>{translations[language]["Get Access to our exclusive party packs."]}</Text>
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontSize: 16,
+                lineHeight: 21,
+                color: "white",
+                flexWrap: "wrap",
+                marginLeft: 54,
+              }}
+            >
+              {
+                translations[language][
+                  "Get Access to our exclusive party packs."
+                ]
+              }
+            </Text>
           </View>
           <View style={styles.info}>
             <View style={styles.infoTitle}>
-              <Image style={{width: 36, height: 36, marginRight: 18}} source={require('../assets/certified-ribbon-1.png')}/>
-              <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 24, lineHeight: 29, color: 'white'}}>{translations[language]["One time payment!"]}</Text>
+              <Image
+                style={{ width: 36, height: 36, marginRight: 18 }}
+                source={require("../assets/certified-ribbon-1.png")}
+              />
+              <Text
+                style={{
+                  fontFamily: "Gilroy-ExtraBold",
+                  fontSize: 24,
+                  lineHeight: 29,
+                  color: "white",
+                }}
+              >
+                {translations[language]["One time payment!"]}
+              </Text>
             </View>
-            <Text style={{fontFamily: 'Poppins', fontSize: 16, lineHeight: 21, color: 'white', flexWrap: 'wrap', marginLeft: 54}}>{translations[language]["Fuck subscription plans. You’ll only pay once for all."]}</Text>
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontSize: 16,
+                lineHeight: 21,
+                color: "white",
+                flexWrap: "wrap",
+                marginLeft: 54,
+              }}
+            >
+              {
+                translations[language][
+                  "Fuck subscription plans. You’ll only pay once for all."
+                ]
+              }
+            </Text>
           </View>
           <View style={styles.info}>
             <View style={styles.infoTitle}>
-              <Image style={{width: 36, height: 36, marginRight: 18}} source={require('../assets/target-miss.png')}/>
-              <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 24, lineHeight: 29, color: 'white'}}>{translations[language]["No more ads!"]}</Text>
+              <Image
+                style={{ width: 36, height: 36, marginRight: 18 }}
+                source={require("../assets/target-miss.png")}
+              />
+              <Text
+                style={{
+                  fontFamily: "Gilroy-ExtraBold",
+                  fontSize: 24,
+                  lineHeight: 29,
+                  color: "white",
+                }}
+              >
+                {translations[language]["No more ads!"]}
+              </Text>
             </View>
-            <Text style={{fontFamily: 'Poppins', fontSize: 16, lineHeight: 21, color: 'white', flexWrap: 'wrap', marginLeft: 54}}>{translations[language]["As a thank you, we release you from the torments of advertising."]}</Text>
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontSize: 16,
+                lineHeight: 21,
+                color: "white",
+                flexWrap: "wrap",
+                marginLeft: 54,
+              }}
+            >
+              {
+                translations[language][
+                  "As a thank you, we release you from the torments of advertising."
+                ]
+              }
+            </Text>
           </View>
         </View>
 
         <View style={styles.footer}>
-          <CustomButton style={{marginBottom: 16, width: '100%'}} onPress={() => purchasePremium()} text={`${price} Upgrade`}/>
-          <TouchableOpacity onPress={() => restorePurchase()} activeOpacity={.7}>
-            <Text style={{fontFamily: 'Poppins', fontSize: 17, lineHeight: 20, color: 'white', marginBottom: 39}}>{translations[language]["Restore previous purchases"]}</Text>
+          <CustomButton
+            style={{ marginBottom: 16, width: "100%" }}
+            onPress={() => purchasePremium()}
+            text={`${price} Upgrade`}
+          />
+          <TouchableOpacity
+            onPress={() => restorePurchase()}
+            activeOpacity={0.7}
+          >
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontSize: 17,
+                lineHeight: 20,
+                color: "white",
+                marginBottom: 39,
+              }}
+            >
+              {translations[language]["Restore previous purchases"]}
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => Linking.openURL('https://gameoftruth.app/privacy/')} activeOpacity={.7}>
-            <Text style={{fontFamily: 'Poppins', fontSize: 12, lineHeight: 14, color: 'white'}}>{translations[language]["Terms of use - Privacy policy"]}</Text>
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL(
+                "https://c2h6ethan.github.io/gameOfTruth.github.io/"
+              )
+            }
+            activeOpacity={0.7}
+          >
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontSize: 12,
+                lineHeight: 14,
+                color: "white",
+              }}
+            >
+              {translations[language]["Terms of use - Privacy policy"]}
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -154,12 +303,11 @@ export default function PaywallScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-
   container: {
-    flex: 1,  
-    backgroundColor: '#6D6AFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "#6D6AFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
   closeButton: {
     height: 46,
@@ -168,25 +316,25 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   content: {
-    width: '80%',
-    height: '100%',
+    width: "80%",
+    height: "100%",
   },
   infoContainer: {
-    position: 'absolute', 
-    top: '33%',
-    justifyContent: 'center',
+    position: "absolute",
+    top: "33%",
+    justifyContent: "center",
   },
   info: {
-    marginBottom: 24
+    marginBottom: 24,
   },
   infoTitle: {
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   footer: {
-    position: 'absolute',
-    top: '75%',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    position: "absolute",
+    top: "75%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,43 +1,172 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Modal, Text, View, Image, SafeAreaView, TouchableOpacity, Linking } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const translations = require('../translations.json');
-
+import {
+  StyleSheet,
+  Modal,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const translations = require("../translations.json");
 
 export default function SettingsScreen({ navigation }) {
   const [language, setLanguage] = useState("en");
 
   useEffect(async () => {
-    var language = await AsyncStorage.getItem('language')
-    if(language){setLanguage(language)}
+    var language = await AsyncStorage.getItem("language");
+    if (language) {
+      setLanguage(language);
+    }
   }, []);
 
   return (
     <View style={styles.container}>
-      <Image style={{position: 'absolute', top: 0, width: '100%'}} source={require('../assets/settingsBackground1.png')}/>
-      <Image style={{position: 'absolute', bottom: 0, width: '100%'}} source={require('../assets/settingsBackground2.png')}/>
+      <Image
+        style={{ position: "absolute", top: 0, width: "100%" }}
+        source={require("../assets/settingsBackground1.png")}
+      />
+      <Image
+        style={{ position: "absolute", bottom: 0, width: "100%" }}
+        source={require("../assets/settingsBackground2.png")}
+      />
       <View style={styles.content}>
-        <TouchableOpacity onPress={() => navigation.goBack()} activeOpacity={.7}>
-          <Image style={styles.closeButton} source={require('../assets/closeButton.png')} /> 
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          activeOpacity={0.7}
+        >
+          <Image
+            style={styles.closeButton}
+            source={require("../assets/closeButton.png")}
+          />
         </TouchableOpacity>
 
-        <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 36, lineHeight: 46, color: 'white', marginBottom: 32}}>{translations[language]["Settings"]}</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('LanguageSettingsScreen')} style={styles.languageSettingsButton}  activeOpacity={.7}>
-          <Text style={{fontFamily: 'Poppins-SemiBold', fontSize: 17, lineHeight: 21, color: 'white', marginLeft: 24}}>{translations[language]["Language Settings"]}</Text>
-          <Image style={{width: 26, height: 26, marginRight: 24}} source={require('../assets/languageSettingsButton.png')} /> 
+        <Text
+          style={{
+            fontFamily: "Gilroy-ExtraBold",
+            fontSize: 36,
+            lineHeight: 46,
+            color: "white",
+            marginBottom: 32,
+          }}
+        >
+          {translations[language]["Settings"]}
+        </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("LanguageSettingsScreen")}
+          style={styles.languageSettingsButton}
+          activeOpacity={0.7}
+        >
+          <Text
+            style={{
+              fontFamily: "Poppins-SemiBold",
+              fontSize: 17,
+              lineHeight: 21,
+              color: "white",
+              marginLeft: 24,
+            }}
+          >
+            {translations[language]["Language Settings"]}
+          </Text>
+          <Image
+            style={{ width: 26, height: 26, marginRight: 24 }}
+            source={require("../assets/languageSettingsButton.png")}
+          />
         </TouchableOpacity>
 
-        <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 30, lineHeight: 36, color: 'white'}}>{translations[language]["How it's played"]}</Text>
-        <Text style={{fontFamily: 'Poppins', fontSize: 17, lineHeight: 22, color: 'white'}}>{translations[language]["A random card with a question or task appears on the screen."]}</Text>
-        <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 24, lineHeight: 31, color: 'white', marginTop: 24}}>{translations[language]["If you answer:"]}</Text>
-        <Text style={{fontFamily: 'Poppins', fontSize: 17, lineHeight: 22, color: 'white'}}>{translations[language]["You get a point."]}</Text>
-        <Text style={{fontFamily: 'Gilroy-ExtraBold', fontSize: 24, lineHeight: 31, color: 'white', marginTop: 24}}>{translations[language]["If you pass:"]}</Text>
-        <Text style={{fontFamily: 'Poppins', fontSize: 17, lineHeight: 22, color: 'white'}}>{translations[language]["You get punished MUHAHAHA!"]}</Text>
+        <Text
+          style={{
+            fontFamily: "Gilroy-ExtraBold",
+            fontSize: 30,
+            lineHeight: 36,
+            color: "white",
+          }}
+        >
+          {translations[language]["How it's played"]}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Poppins",
+            fontSize: 17,
+            lineHeight: 22,
+            color: "white",
+          }}
+        >
+          {
+            translations[language][
+              "A random card with a question or task appears on the screen."
+            ]
+          }
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Gilroy-ExtraBold",
+            fontSize: 24,
+            lineHeight: 31,
+            color: "white",
+            marginTop: 24,
+          }}
+        >
+          {translations[language]["If you answer:"]}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Poppins",
+            fontSize: 17,
+            lineHeight: 22,
+            color: "white",
+          }}
+        >
+          {translations[language]["You get a point."]}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Gilroy-ExtraBold",
+            fontSize: 24,
+            lineHeight: 31,
+            color: "white",
+            marginTop: 24,
+          }}
+        >
+          {translations[language]["If you pass:"]}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Poppins",
+            fontSize: 17,
+            lineHeight: 22,
+            color: "white",
+          }}
+        >
+          {translations[language]["You get punished MUHAHAHA!"]}
+        </Text>
 
-        <TouchableOpacity onPress={() => Linking.openURL('https://gameoftruth.app/privacy/')} style={styles.termsButton} activeOpacity={.7}>
-          <Text style={{fontFamily: 'Poppins', fontSize: 17, lineHeight: 20, color: 'white'}}>{translations[language]["Terms of use - Privacy policy"]}</Text>
-          <Image style={{width: 26, height: 26}} source={require('../assets/arrowWhite.png')} /> 
+        <TouchableOpacity
+          onPress={() =>
+            Linking.openURL(
+              "https://c2h6ethan.github.io/gameOfTruth.github.io/"
+            )
+          }
+          style={styles.termsButton}
+          activeOpacity={0.7}
+        >
+          <Text
+            style={{
+              fontFamily: "Poppins",
+              fontSize: 17,
+              lineHeight: 20,
+              color: "white",
+            }}
+          >
+            {translations[language]["Terms of use - Privacy policy"]}
+          </Text>
+          <Image
+            style={{ width: 26, height: 26 }}
+            source={require("../assets/arrowWhite.png")}
+          />
         </TouchableOpacity>
       </View>
     </View>
@@ -45,42 +174,41 @@ export default function SettingsScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-
   container: {
-    flex: 1,  
-    backgroundColor: '#111111',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: "#111111",
+    alignItems: "center",
+    justifyContent: "center",
   },
   closeButton: {
     height: 46,
     width: 46,
     marginTop: 60,
-    marginBottom: 58
+    marginBottom: 58,
   },
   content: {
-    width: '80%',
-    height: '100%'
+    width: "80%",
+    height: "100%",
   },
   languageSettingsButton: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '100%',
-    borderColor: 'white',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    borderColor: "white",
     borderWidth: 2,
-    borderStyle: 'solid',
+    borderStyle: "solid",
     height: 58,
     borderRadius: 10,
-    marginBottom: 102
+    marginBottom: 102,
   },
   termsButton: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent:  'space-between',
-    width: '100%',
-    marginTop: 66
-  }
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: 66,
+  },
 });
